f7ae6a4afda43cb348b70be358da194ae082685c
Matthew Tan <tanmatt804@gmail.com>
2021-11-03 14:48:01 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //write your code here
+const reverseString = (string) => {
+    let stringHold = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        stringHold += string[i];
+    }
+    return stringHold;
+} 
+
+console.log(reverseString("Matthew"));
